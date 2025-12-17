753f35798edc4d6039a3cf322951be59083c43a8
Badal Sahoo <badalsahoo@Badals-MacBook-Air.local>
2025-12-17 14:52:08 +0530

    added home.jsx

---
+++ client/src/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+        <p>HOME</p>
+     </div>
+    </>
+  )
+}
+
+export default Home
