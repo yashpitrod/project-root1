a4380c349333910b97c978369839bd48d94cf0ef
yashpitrod <pitrodayash1412007@gmail.com>
2025-12-18 14:17:00 +0530

    Added ui for Authentication- Login and Register Page

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../auth/Login';
+import Register from '../auth/Register';
+import StudentDashboard from '../pages/StudentDashboard';
+import DoctorDashboard from '../pages/DoctorDashboard';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/doctor" element={<DoctorDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
