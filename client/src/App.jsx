05e7d4183009d8083df0c09411f4b2133bb0de1c
siddhi biyani <siddhibiyani@gmail.com>
2025-12-19 12:39:44 +0530

    app.jsx changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import AppRouter from './router/AppRouter';
 
+import StudentDashboard from "./StudentDashboard";
+
 const App = () => {
   return <AppRouter />;
 };
+function App() {
+  return <StudentDashboard />;
+}
 
 export default App;
