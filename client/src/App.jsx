d617f7dfa78765b7d63f2fd8667cf822b659a33e
siddhi biyani <siddhibiyani@gmail.com>
2025-12-19 12:47:27 +0530

    router and app file changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,7 @@
 import AppRouter from './router/AppRouter';
 
-import StudentDashboard from "./StudentDashboard";
-
 const App = () => {
   return <AppRouter />;
 };
-function App() {
-  return <StudentDashboard />;
-}
 
 export default App;
